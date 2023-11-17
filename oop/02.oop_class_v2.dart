dc00b61e54b272a0017e2cbb15e025548ea91023
littleboyaba <northlightingstar@gmail.com>
2023-11-18 04:10:46 +0600

    update and started OOP in dart

--- oop/02.oop_class_v2.dart
+++ oop/02.oop_class_v2.dart
@@ -1,9 +1,6 @@
 void main(){
   // object/instance creation
-  Student studentOne = Student();
-  studentOne.name = 'Jack Jon';
-  studentOne.age = 27;
-  studentOne.address = 'Dhaka, Bangladesh';
+  Student studentOne = Student('Jack Jon', 27, 'Dhaka, Bangladesh');
 
   print(studentOne.name);
   print(studentOne.age);
@@ -11,28 +8,55 @@ void main(){
   studentOne.coding();
   studentOne.doing('watching bard');
 
-  Student studentTwo = Student();
-  studentTwo.name = 'Khalid';
-  studentTwo.age = 35;
-  studentTwo.address = 'Uttara, Dhaka';
+  Student studentTwo = Student('Khalid', 35, 'Uttara, Dhaka');
 
   print(studentTwo.name);
   print(studentTwo.age);
   print(studentTwo.address);
   studentTwo.coding();
   studentTwo.doing('homework');
+
   int result = studentTwo.add(34, 23);
   print(result);
 
+  Student studentThree = Student('Rock', 39, 'UK');
+
+  print(studentThree.name);
+  print(studentThree.age);
+  print(studentThree.address);
+
+  print(Student.noOfObject);
+  print(Student.getClassName());
 
 }
 
 // class
 class Student {
   // properties/attributes
-  String name = '';
-  int age = 0;
-  String address = '';
+  late String name;
+  late int age;
+  late String address;
+
+  static int noOfObject = 0;
+
+  // construct the object of this class.
+  // constructor
+  // to use class variable inside constructor parameter use this.classVariable name
+  // you can do it other way too. like
+  // we use (this.) to identify classVariableName is the parameter name and class variable names are same.
+  /*
+  Student(parameter01, parameter02, parameter03){
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+   */
+  Student(this.name, this.age, this.address){
+    print('==================================');
+    print('The object is creating');
+    print('==================================');
+    noOfObject += 1;
+  }
 
   // Method
   void coding() {
@@ -46,6 +70,10 @@ class Student {
 
   int add(int a, int b){
     return a+b;
+  }
+
+  static String getClassName(){
+    return 'This is student class';
   }
 
 }
