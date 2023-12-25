8aa13933c23ed4b8b11f9c58d643e3f57074744c
littleboyaba <northlightingstar@gmail.com>
2023-12-25 19:06:53 +0600

    code mama problem 3 to 5

--- bin/code_mama/problem_04.dart
+++ bin/code_mama/problem_04.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 
 void main() {
+  List<String> input = stdin.readLineSync()!.split(' ');
+  int l1 = int.parse(input[0]);
+  int r1 = int.parse(input[1]);
+  int l2 = int.parse(input[2]);
+  int r2 = int.parse(input[3]);
 
-  int screenWidth = int.parse(stdin.readLineSync()!);
+  for (int i = l1; i < l2; i++) {
+    stdout.write('$i ');
+  }
 
-  int tableWidth = 300;
+  for (int i = r2 + 1; i <= r1; i++) {
+    stdout.write('$i ');
+  }
 
-  int leftMargin = (screenWidth - tableWidth) ~/ 2;
-
-  print(leftMargin);
+  print('');
 
 }
